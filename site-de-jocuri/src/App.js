55272b3a4dba14b4e3e39d8051a7214fad258b91
AlinTM99 <Alin_m61@yahoo.com>
2022-08-09 22:45:11 +0300

    First Commit

---
+++ site-de-jocuri/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './myApp.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>cand citesti un cuvant urat adauga bip
+     </p>
+    <p className="parteadejos">Ford focus ST</p>
+    </div>
+  );
+}
+
+export default App;
